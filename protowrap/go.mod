ec9a40481a33354127003c787e2aa31342e795bd
Alexander Menshchikov <alexander.menshchikov@yandex.ru>
2024-11-10 20:07:12 +0300

    proto wrappers

---
+++ protowrap/go.mod
@@ -0,0 +1,5 @@
+module github.com/nbgrp/pkg/protowrap
+
+go 1.20
+
+require google.golang.org/protobuf v1.34.2
---
+++ protowrap/wrappers.go
@@ -0,0 +1,21 @@
+package protowrap
+
+import (
+	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// Ptr constructs a pointer to the value of any type.
+func Ptr[T any](v T) *T {
+	return &v
+}
+
+// FromTimePtr constructs an instance of timestamppb.Timestamp from the provided time.Time.
+// If the nil has passed, then the nil will be returned.
+func FromTimePtr(t *time.Time) *timestamppb.Timestamp {
+	if t == nil {
+		return nil
+	}
+	return timestamppb.New(*t)
+}
